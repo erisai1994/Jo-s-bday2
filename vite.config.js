b309c0bc21914e6bb459311d263a77b699e039a9
erisai1994 <axel.scheppers@gmail.com>
2025-08-23 02:31:37 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',     // IMPORTANT : pour que les chemins relatifs fonctionnent
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist'
